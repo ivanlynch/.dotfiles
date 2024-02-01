254403ddc8cba32f70cff536d3bc5054d1bb1866
Ivan Lynch <ivan.lynch@outlook.com.ar>
2024-02-01 07:40:42 -0300

    chore: added nvim config

---
+++ .config/nvim/lua/config/remaps.lua
@@ -0,0 +1,28 @@
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+keymap.set("n", "<leader>e", ":Vex<CR>", opts)
+
+keymap.set("n", "J", ":move -1<CR>", opts)
+
+
+
+
+
+
+
+
+
+keymap.set("n", "K", ":move +1<CR>", opts)
+
+
+
+
+
+
+
+
+
+
+
+
