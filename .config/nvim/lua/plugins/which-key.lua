81ff8022ed1f6863a37586196d86a322076ec251
Ivan Lynch <ivan.lynch@outlook.com.ar>
2025-05-23 15:19:58 -0300

    refactor: update LSP configuration and enhance OS detection in installation script

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -1,48 +1,45 @@
 return {
 	"folke/which-key.nvim",
 	event = "VeryLazy",
+	dependencies = {
+		"echasnovski/mini.icons",
+	},
 	config = function()
-		local opts = {}
-		local mappings = {
-			c = {
-				name = "Code Actions",
-				p = { "<cmd>TroubleToggle<cr>", "Show problems" },
-				i = { "<cmd>Telescope lsp_implementations<cr>", "Go to the implementation" },
-				d = { "<cmd>Telescope lsp_definitions<cr>", "Go to the definition" },
-				r = { "<cmd>Telescope lsp_references show_line=false<cr>", "Show references" },
-				a = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Show code actions" },
-				g = { "<cmd>TSC<cr>", "Show global problems" },
-			},
-			f = {
-				name = "File Actions",
-				f = { "<cmd>Telescope find_files<cr>", "Find File" },
-				r = { "<cmd>Telescope oldfiles<cr>", "Recent Files" },
-				g = { "<cmd>Telescope live_grep<cr>", "Grep" },
-				b = { "<cmd>Telescope buffers<cr>", "Buffers" },
-				h = { "<cmd>Telescope help_tags<cr>", "Help Tags" },
-				s = { "<cmd>Telescope lsp_document_symbols<cr>", "Document Symbols" },
-				S = { "<cmd>Telescope lsp_workspace_symbols<cr>", "Workspace Symbols" },
-				p = { "<cmd>Telescope project<cr>", "Project" },
-			},
-			w = {
-				name = "Window Actions",
-				v = { "<C-w>v", "Split Vertical" },
-				s = { "<C-w>s", "Split Horizontal" },
-				h = { "<C-w>h", "Move Left" },
-				j = { "<C-w>j", "Move Down" },
-				k = { "<C-w>k", "Move Up" },
-				l = { "<C-w>l", "Move Right" },
-			},
-			g = {
-				name = "Git Actions",
-				C = { "<cmd>Telescope git_commits<cr>", "Show all commits" },
-				c = { "<cmd>Telescope git_bcommits<cr>", "Show current file commits" },
-				b = { "<cmd>Telescope git_branches<cr>", "Show branches" },
-				s = { "<cmd>Telescope git_status<cr>", "Show status" },
-				t = { "<cmd>Telescope git_stash<cr>", "Show stash" },
-			},
-		}
-		require("which-key").setup(opts)
-		require("which-key").register(mappings, { prefix = "<leader>" })
+		require("which-key").setup({})
+		local wk = require("which-key")
+		wk.register({
+			["<leader>c"] = { name = "Code Actions" },
+			["<leader>ca"] = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Show code actions" },
+			["<leader>cd"] = { "<cmd>Telescope lsp_definitions<cr>", "Go to the definition" },
+			["<leader>cg"] = { "<cmd>TSC<cr>", "Show global problems" },
+			["<leader>ci"] = { "<cmd>Telescope lsp_implementations<cr>", "Go to the implementation" },
+			["<leader>cp"] = { "<cmd>TroubleToggle<cr>", "Show problems" },
+			["<leader>cr"] = { "<cmd>Telescope lsp_references show_line=false<cr>", "Show references" },
+
+			["<leader>f"] = { name = "File Actions" },
+			["<leader>fS"] = { "<cmd>Telescope lsp_workspace_symbols<cr>", "Workspace Symbols" },
+			["<leader>fb"] = { "<cmd>Telescope buffers<cr>", "Buffers" },
+			["<leader>ff"] = { "<cmd>Telescope find_files<cr>", "Find File" },
+			["<leader>fg"] = { "<cmd>Telescope live_grep<cr>", "Grep" },
+			["<leader>fh"] = { "<cmd>Telescope help_tags<cr>", "Help Tags" },
+			["<leader>fp"] = { "<cmd>Telescope project<cr>", "Project" },
+			["<leader>fr"] = { "<cmd>Telescope oldfiles<cr>", "Recent Files" },
+			["<leader>fs"] = { "<cmd>Telescope lsp_document_symbols<cr>", "Document Symbols" },
+
+			["<leader>g"] = { name = "Git Actions" },
+			["<leader>gC"] = { "<cmd>Telescope git_commits<cr>", "Show all commits" },
+			["<leader>gb"] = { "<cmd>Telescope git_branches<cr>", "Show branches" },
+			["<leader>gc"] = { "<cmd>Telescope git_bcommits<cr>", "Show current file commits" },
+			["<leader>gs"] = { "<cmd>Telescope git_status<cr>", "Show status" },
+			["<leader>gt"] = { "<cmd>Telescope git_stash<cr>", "Show stash" },
+
+			["<leader>w"] = { name = "Window Actions" },
+			["<leader>wh"] = { "<C-w>h", "Move Left" },
+			["<leader>wj"] = { "<C-w>j", "Move Down" },
+			["<leader>wk"] = { "<C-w>k", "Move Up" },
+			["<leader>wl"] = { "<C-w>l", "Move Right" },
+			["<leader>wv"] = { "<C-w>v", "Split Vertical" },
+			["<leader>ws"] = { "<C-w>s", "Split Horizontal" },
+		})
 	end,
 }
