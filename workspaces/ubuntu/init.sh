435f22be515f596081ff3863c847a0e07b983c96
Ivan Lynch <Ivan.lynch@ab-inbev.com>
2025-05-25 16:40:48 -0300

    refactor: improve init script for directory removal and copying
    - Updates the init script to use the `-f` flag with `rm` and `cp` commands for forceful removal and copying, enhancing robustness.
    - Ensures that existing directories are removed without prompts and copied recursively, improving the initialization process for Ansible and configuration files.
    - Streamlines the script for better error handling and efficiency.

--- workspaces/ubuntu/init.sh
+++ workspaces/ubuntu/init.sh
@@ -6,22 +6,22 @@ cd "$(dirname "$0")"
 # Eliminar directorio ansible local si existe
 if [[ -d ansible ]]; then
    echo "Eliminando directorio ansible existente..."
-   rm -r ./ansible 
+   rm -rf ./ansible 
 fi
 
 if [[ -d .config ]]; then
    echo "Eliminando directorio .config existente"
-   rm -r ./.config
+   rm -rf ./.config
 fi
 
 # Verificar si existe el directorio ansible en home y copiarlo
 if [[ -d ~/ansible ]]; then
    echo "Copiando directorio ansible desde $HOME/ansible..."
-   cp -r ~/ansible ansible
+   cp -rf ~/ansible ansible
 # Si no existe en home, verificar si existe en el repositorio dotfiles
 elif [[ -d ~/.dotfiles/ansible ]]; then
    echo "Copiando directorio ansible desde el repositorio dotfiles..."
-   cp -r ~/.dotfiles/ansible ansible
+   cp -rf ~/.dotfiles/ansible ansible
 fi
 
 pwd
@@ -30,22 +30,22 @@ pwd
 if [[ -d ~/.config/nvim ]]; then
    echo "Copiando directorio .config/nvim desde $HOME/.config/nvim..."
    mkdir .config
-   cp -r ~/.config/nvim .config/nvim
+   cp -rf ~/.config/nvim .config/nvim
 # Si no existe en home, verificar si existe en el repositorio dotfiles
 elif [[ -d ~/.dotfiles/.config/nvim ]]; then
    echo "Copiando directorio .config/nvim desde el repositorio dotfiles..."
    mkdir .config
-   cp -r ~/.dotfiles/.config/nvim .config/nvim
+   cp -rf ~/.dotfiles/.config/nvim .config/nvim
 fi
 
 # Verificar si existe configuracion de Neovim
 if [[ -d ~/.config/fish ]]; then
    echo "Copiando directorio .config/fish desde $HOME/.config/fish..."
-   cp -r ~/.config/fish .config/fish
+   cp -rf ~/.config/fish .config/fish
 # Si no existe en home, verificar si existe en el repositorio dotfiles
 elif [[ -d ~/.dotfiles/.config/fish ]]; then
    echo "Copiando directorio .config/fish desde el repositorio dotfiles..."
-   cp -r ~/.dotfiles/.config/fish .config/fish
+   cp -rf ~/.dotfiles/.config/fish .config/fish
 fi
 
 # Verificar si existe el Dockerfile
