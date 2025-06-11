bba8963e8cef35701697649318e7bf470b5c25f1
Ivan Lynch <Ivan.lynch@ab-inbev.com>
2025-06-11 08:49:45 -0300

    feat: add .gitconfig and configure Git safe directories for Neovim plugins in entrypoint.sh and Dockerfile

--- workspaces/alpine/entrypoint.sh
+++ workspaces/alpine/entrypoint.sh
@@ -6,6 +6,20 @@ if [ ! -z "$GIT_USER_NAME" ] && [ ! -z "$GIT_USER_EMAIL" ]; then
     git config --global user.email "$GIT_USER_EMAIL"
 fi
 
+# Configure Git safe directories for Neovim plugins
+git config --global --add safe.directory "/home/$USER_NAME/.local/share/nvim/lazy/lazy.nvim"
+git config --global --add safe.directory "/home/$USER_NAME/.local/share/nvim/lazy/conform.nvim"
+git config --global --add safe.directory "/home/$USER_NAME/.local/share/nvim/lazy/fzf-lua"
+git config --global --add safe.directory "/home/$USER_NAME/.local/share/nvim/lazy/nvim-treesitter"
+git config --global --add safe.directory "/home/$USER_NAME/.local/share/nvim/lazy/nvim-web-devicons"
+git config --global --add safe.directory "/home/$USER_NAME/.local/share/nvim/lazy/*"
+git config --global --add safe.directory "/home/$USER_NAME/workspace"
+git config --global --add safe.directory "/home/$USER_NAME/workspace/*"
+git config --global --add safe.directory "/home/$USER_NAME/workspaces"
+git config --global --add safe.directory "/home/$USER_NAME/workspaces/*"
+git config --global --add safe.directory "/home/$USER_NAME/.dotfiles"
+git config --global --add safe.directory "*"
+
 # Change user uid to host user's uid
 if [ ! -z "$USER_ID" ] && [ "$(id -u $USER_NAME)" != "$USER_ID" ]; then
     # Create the user group if it does not exist
