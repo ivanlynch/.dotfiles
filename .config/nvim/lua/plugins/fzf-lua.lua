32d692fb693a64d3d3fe20bf6c89e580b1aff6b2
Ivan Lynch <your-email@example.com>
2025-06-11 20:56:53 -0300

    feat: configure fzf-lua keymappings

--- .config/nvim/lua/plugins/fzf-lua.lua
+++ .config/nvim/lua/plugins/fzf-lua.lua
@@ -2,5 +2,42 @@
 return {
   "ibhagwan/fzf-lua",
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  opts = {}
+  opts = {},
+  keys = {
+    {
+      "<leader>ff",
+      function()
+        require('fzf-lua').files()
+      end,
+      desc="[F]ind [F]iles"
+    },
+    {
+      "<leader>fg",
+      function()
+        require('fzf-lua').live_grep()
+      end,
+      desc="[F]ind using [G]rep"
+    },
+    {
+      "<leader>fb",
+      function()
+        require('fzf-lua').buffers()
+      end,
+      desc="[F]ind [B]uffers"
+    },
+    {
+      "<leader>fh",
+      function()
+        require('fzf-lua').oldfiles()
+      end,
+      desc="[F]ind [H]istory"
+    },
+    {
+      "<leader>fk",
+      function()
+        require('fzf-lua').keymaps()
+      end,
+      desc="[F]ind [K]eymappings"
+    },
+  }
 }
