c8e6e29695a62de8b6ef4fe83db632654d282ccd
Ivan Lynch <your-email@example.com>
2025-06-17 18:02:49 -0300

    fix: enable copy to clipboard when yank

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,6 +2,7 @@ vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
+vim.opt.clipboard = "unnamedplus"
 
 vim.opt.swapfile = false
 vim.g.loaded_node_provider = 0
